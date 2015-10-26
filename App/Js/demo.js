7b54bc7f6b7db4eac5c6c88bf6eed7ebe0b8e5ee
Anil Kumar <anil.akbp@gmail.com>
2015-10-26 10:29:37 -0700

    Call Recordings Download Fix

--- App/Js/demo.js
+++ App/Js/demo.js
@@ -1,4 +1,4 @@
-
+    
     
     var callrecording;
 
@@ -34,6 +34,7 @@
         }
         t.authorize = function(username,extension,password) {
             p = t.rcsdk.getPlatform();
+            s = t.rcsdk;
             p.authorize({
                 username:  username, // phone number in full format
                 extension: extension, // leave blank if direct number is used
@@ -44,14 +45,15 @@
             }).catch(function(e) {
                 alert('ERR ' + e.message  || 'Server cannot authorize user');
             });
-            t.rch = new rcHelper(p);
+            t.rch = new rcHelper(p,s);
         }
     }
 
-    function rcHelper(platform) {
+    function rcHelper(platform,rcsdk) {
         var t=this;
         // var callrecording;
         t.platform = platform;
+        t.rcsdk = rcsdk;
         t.send_sms = function(from,to,text) {
             t.platform.post('/account/~/extension/~/sms', {
                 body: {
@@ -151,16 +153,50 @@
                                     t.platform.get(url).then(function(response){
                                         // response.setHeader('Content-Type':'audio/x-wav');
                                         alert('Success: ' + response.data);
+                                        var donwloadLink = document.getElementById("download_call_recordings");
+                                        donwloadLink.href = "data:audio/mpeg;base64,"+window.btoa(response.data);
+                                        // var donwloadLink = btoa()
+                                        console.log(response.data);
+                                        console.log(donwloadLink.href);
+                                        // console.log()
                                         // alert('Response Header' + response.);
                                     }).catch(function(e){
                                         alert('Error' + e.message);
                                     });
 
                              
-                  }
-              }
-              }
-      }           
+                        }
+                }
+        }
+        t.startSubscription = function () {
+            console.log("inside subscription");
+            t.subscription = t.rcsdk.getSubscription();
+            console.log(t.rcsdk);
+            console.log(t.subscription);
+            t.subscription
+            .on(t.subscription.events.notification, function(msg) {
+                conosle.log("inside on function");
+                var callerId = msg.body.activeCalls[0].from;
+                console.log(msg.body.activeCalls[0].from); // activeCalls is array
+                console.log(msg.body.activeCalls[0].to);
+                var call0 = msg.body.activeCalls[0];
+                var json = JSON.stringify(call0);
+                document.getElementById("subscription").innerHTML=txt;
+                console.log(json);
+                rcCallWin.processCall(call0);
+            })
+            .register({
+                events: [
+                    '/account/~/extension/~/presence?detailedTelephonyState=true'
+                ],
+            })
+            .then();
+        }
+    }
+
+
+
+          
 
 
 rcm = new rcManager('#appkey','#appSecret','#userid','#extension','#password');
